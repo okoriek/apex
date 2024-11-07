2635ce49aee4d583b6e0acc0b0633af1fc847141
okoriek <okoriek55@gmail.com>
2024-11-07 05:44:15 +0100

    settings update

--- website/settings.py
+++ website/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-3mjg9&2kidb4ix+b8$lq=@&t#9+%c_5407gobdv#x)u8xseojf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.Apexfortitude.com','Apexfortitude.com', 'cron-job.org', '127.0.0.1']
+ALLOWED_HOSTS = ['www.Apexfortitude.com','Apexfortitude.com', 'cron-job.org', '*']
 
 
 # Application definition
